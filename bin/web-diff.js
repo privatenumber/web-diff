431d75477951c1fe1fbb2ec5eca3819968d33370
Lance <38918961+rancerun@users.noreply.github.com>
2019-07-24 04:04:02 -0400

    Fixed spacing issues
    Changed tab indentation to 2 spaces instead

--- bin/web-diff.js
+++ bin/web-diff.js
@@ -3,20 +3,20 @@
 const webDiff = require('../lib');
 
 const { argv } = require('yargs')
-	.scriptName('web-diff')
-	.usage('Usage: $0 <URL A> <URL B>')
-	.demandCommand(2)
-	.options({
-		'output': {
-			alias: 'o',
-			describe: 'Path to save the diff image to',
-			string: true,
-		},
-		'verbose': {
-			alias: 'v',
-			describe: 'Verbose output',
-			boolean: true,			
-		},
-	});
+  .scriptName('web-diff')
+  .usage('Usage: $0 <URL A> <URL B>')
+  .demandCommand(2)
+  .options({
+    'output': {
+      alias: 'o',
+      describe: 'Path to save the diff image to',
+      string: true,
+    },
+    'verbose': {
+      alias: 'v',
+      describe: 'Verbose output',
+      boolean: true,			
+    },
+  });
 
 webDiff(argv);
